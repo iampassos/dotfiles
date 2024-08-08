632f4cf61d2e5961b8773b203ce8581b8bbe76ef
iampassos <78429887+iampassos@users.noreply.github.com>
2024-08-08 13:43:06 -0300

    Refactored and fixed keymap

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -69,14 +69,21 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "echasnovski/mini.icons" },
     config = function()
-      require("oil").setup({
+      local oil = require("oil")
+
+      oil.setup({
         columns = { "icon" },
         view_options = {
           show_hidden = true,
         },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<C-p>"] = false,
+        },
       })
 
-      vim.keymap.set("n", "<space><TAB>", require("oil").toggle_float)
+      vim.keymap.set("n", "<space><TAB>", oil.toggle_float)
+      vim.keymap.set("n", "<space>p", oil.open_preview)
     end,
   },
 }
