63d99f91e238f995973be03abb165c9b3de8c16a
iampassos <78429887+iampassos@users.noreply.github.com>
2025-08-19 17:07:15 -0300

    changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,12 +42,12 @@ vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>")
 -- MISC
 --
 
-vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+vim.api.nvim_create_autocmd("BufWinLeave", {
   pattern = { "*.*" },
   command = "mkview",
 })
 
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = { "*.*" },
   command = "silent! loadview",
 })
