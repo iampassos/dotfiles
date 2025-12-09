ef659d10dca580278f6bda904c42271b40becad7
iampassos <78429887+iampassos@users.noreply.github.com>
2025-12-09 11:26:24 -0300

    New config starting

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo dnf upgrade --refresh -y
+
+sudo dnf install -y \
+    git vim stow zsh tmux fzf maim xclip xsel fd-find ripgrep xsetroot bat \
+    zip unzip jetbrains-mono-fonts-all @development-tools
+
+[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+sudo dnf install -y flatpak 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub us.zoom.Zoom
+
+curl -fsS https://dl.brave.com/install.sh | sh
+
+command -v rustc >/dev/null 2>&1 || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+command -v bob >/dev/null 2>&1 || cargo install bob-nvim
+bob use nightly
+
+sudo dnf copr enable scottames/ghostty -y
+sudo dnf install ghostty -y
+
+[ -d "$HOME/.config/i3" ] && rm -rf "$HOME/.config/i3"
+[ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.default"
+[ ! -d "$HOME/.dotfiles" ] && git clone https://github.com/iampassos/dotfiles "$HOME/.dotfiles"
+[ -d "$HOME/.dotfiles" ] && cd "$HOME/.dotfiles" && stow . && cd "$HOME"
+
+[ "$SHELL" != "$(which zsh)" ] && chsh -s $(which zsh)
